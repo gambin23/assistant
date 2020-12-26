e530013105ed24d1398cd5be4bc020aaf83af826
Gilbert Gambin <gilbertgambin@gmail.com>
2020-12-26 08:53:12 +0100

    feat(sidebar): initial sidebar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import { IntlProvider } from "react-intl";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import classNames from "classnames";
+import { Grid1x2Fill } from "react-bootstrap-icons";
 
 import store from "./store/Store";
 import TransactionsPage from "./ui/TransactionsPage";
@@ -9,24 +11,58 @@ import AddTransactionPage from "./ui/AddTransactionPage";
 import TransactionPage from "./ui/TransactionPage";
 
 function App() {
+    const [hideMenu, setHideMenu] = useState(false);
+
+    function toggleMenu() {
+        setHideMenu(!hideMenu);
+    }
+
     return (
         <Provider store={store}>
             <IntlProvider locale="en-GB">
                 <Router>
-                    <ul>
-                        <li>
-                            <Link to="/">Transactions</Link>
-                        </li>
-                        <li>
-                            <Link to="/new">Add Transaction</Link>
-                        </li>
-                    </ul>
-                    <div className="container">
-                        <Switch>
-                            <Route path="/new" component={AddTransactionPage} />
-                            <Route path="/transaction/:id" component={TransactionPage} />
-                            <Route path="/" component={TransactionsPage} />
-                        </Switch>
+                    <div className={classNames("d-flex", { toggled: hideMenu })} id="wrapper">
+                        <div className="bg-light border-right" id="sidebar-wrapper">
+                            <div className="sidebar-heading">Assistant</div>
+                            <div className="list-group list-group-flush">
+                                <Link to="/" className="list-group-item list-group-item-action bg-light">Transactions</Link>
+                                <Link to="/new" className="list-group-item list-group-item-action bg-light">Add Transaction</Link>
+                                <Link to="/" className="list-group-item list-group-item-action bg-light">Dashboard</Link>
+                                <Link to="/" className="list-group-item list-group-item-action bg-light">Settings</Link>
+                                <Link to="/" className="list-group-item list-group-item-action bg-light">Logout</Link>
+                            </div>
+                        </div>
+
+                        <div id="page-content-wrapper">
+                            <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                                <Grid1x2Fill onClick={toggleMenu} id="menu-btn"></Grid1x2Fill>
+                                <div className="navbar-collapse">
+                                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                                        <li className="nav-item">
+                                            <Link to="/" className="nav-link">Account</Link>
+                                        </li>
+                                        <li className="nav-item dropdown">
+                                            <Link to="/" className="nav-link dropdown-toggle" role="button">
+                                                Notifications
+                                            </Link>
+                                            <div className="dropdown-menu dropdown-menu-right">
+                                                <Link to="/" className="dropdown-item">Transaction</Link>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+
+
+                            <div className="container">
+                                {hideMenu && 'Assistant'}
+                                <Switch>
+                                    <Route path="/new" component={AddTransactionPage} />
+                                    <Route path="/transaction/:id" component={TransactionPage} />
+                                    <Route path="/" component={TransactionsPage} />
+                                </Switch>
+                            </div>
+                        </div>
                     </div>
                 </Router>
             </IntlProvider>
--- src/ui/AddTransactionPage.tsx
+++ src/ui/AddTransactionPage.tsx
@@ -26,53 +26,57 @@ const AddTransactionPage = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h2>Add Transaction</h2>
+        <div className="card">
+            <div className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h2>Add Transaction</h2>
 
-            <div className="form-group">
-                <input type="number" name="amount"
-                    className={classNames('form-control', { 'is-invalid': errors.type })}
-                    ref={register({ valueAsNumber: true, required: "Amount is required" })} />
+                    <div className="form-group">
+                        <input type="number" name="amount"
+                            className={classNames('form-control', { 'is-invalid': errors.type })}
+                            ref={register({ valueAsNumber: true, required: "Amount is required" })} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Entity</label>
+                        <input type="text" name="entity" placeholder="Enter entity"
+                            className={classNames('form-control', { 'is-invalid': errors.entity })}
+                            ref={register({ required: "Entity is required" })} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Description (Optional)</label>
+                        <input type="text" className="form-control" name="description" placeholder="Enter description" ref={register} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Type</label>
+                        <select name="type" defaultValue=""
+                            className={classNames('form-control', { 'is-invalid': errors.type })}
+                            ref={register({ required: "Type is required" })}>
+                            <option value="" disabled>Select…</option>
+                            {Object.keys(types).map(key => (
+                                <option value={key} key={key}>{types[key]}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Category</label>
+                        <select name="category" defaultValue=""
+                            className={classNames('form-control', { 'is-invalid': errors.category })}
+                            ref={register({ required: "Category is required" })}>
+                            <option value="" disabled>Select…</option>
+                            {Object.keys(categories).map(key => (
+                                <option value={key} key={key}>{categories[key]}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <input type="submit" value="Submit" className="btn btn-primary" />
+                </form>
             </div>
-
-            <div className="form-group">
-                <label>Entity</label>
-                <input type="text" name="entity" placeholder="Enter entity"
-                    className={classNames('form-control', { 'is-invalid': errors.entity })}
-                    ref={register({ required: "Entity is required" })} />
-            </div>
-
-            <div className="form-group">
-                <label>Description (Optional)</label>
-                <input type="text" className="form-control" name="description" placeholder="Enter description" ref={register} />
-            </div>
-
-            <div className="form-group">
-                <label>Type</label>
-                <select name="type" defaultValue=""
-                    className={classNames('form-control', { 'is-invalid': errors.type })}
-                    ref={register({ required: "Type is required" })}>
-                    <option value="" disabled>Select…</option>
-                    {Object.keys(types).map(key => (
-                        <option value={key} key={key}>{types[key]}</option>
-                    ))}
-                </select>
-            </div>
-
-            <div className="form-group">
-                <label>Category</label>
-                <select name="category" defaultValue=""
-                    className={classNames('form-control', { 'is-invalid': errors.category })}
-                    ref={register({ required: "Category is required" })}>
-                    <option value="" disabled>Select…</option>
-                    {Object.keys(categories).map(key => (
-                        <option value={key} key={key}>{categories[key]}</option>
-                    ))}
-                </select>
-            </div>
-
-            <input type="submit" value="Submit" className="btn btn-primary" />
-        </form>
+        </div>
     );
 }
 
