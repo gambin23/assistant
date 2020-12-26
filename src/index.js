d6dab297655899745862736751289a3ddac8acc9
Gilbert Gambin <gilbertgambin@gmail.com>
2020-12-26 09:09:55 +0100

    feat(scss): add scss support

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/styles.css';
+import './styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
