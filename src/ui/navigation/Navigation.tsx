9f2eb93cb501abf17acdee9eb7468649fdbce1d8
Gilbert Gambin <gilbertgambin@gmail.com>
2020-12-26 19:19:48 +0100

    refactor(scss): add custom variables file

--- src/ui/navigation/Navigation.tsx
+++ src/ui/navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import classNames from "classnames";
 import { Grid1x2Fill } from "react-bootstrap-icons";
+
 import Routes from "./Routes";
 
 function Navigation() {
