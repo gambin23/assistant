692784fdfba0ba4800e21e8c2932243de8025f1e
Gilbert Gambin <gilbertgambin@gmail.com>
2022-12-18 10:29:12 +0100

    feat(food): on add new recipe create notification

--- src/food/src/store/new-recipe/new-recipe.effects.ts
+++ src/food/src/store/new-recipe/new-recipe.effects.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actions, createEffect, ofType, concatLatestFrom } from '@ngrx/effects';
 import { catchError, map, mergeMap, of, switchMap, tap } from 'rxjs';
-import { alertAddSuccess, UserSelector } from '@assistant/common-sdk';
+import { alertAddSuccess, notificationAdd, UserSelector } from '@assistant/common-sdk';
 import { UserRecipesData } from '@assistant/food/data';
+import { FOOD_APP } from '@assistant/food/name';
 import { NewRecipeSelector } from './new-recipe.selector';
 import { newRecipeAdd, newRecipeAddSuccess, newRecipeAddError } from './new-recipe.actions';
 import { routeFoodRecipe } from './../../routes';
@@ -16,7 +17,12 @@ export class NewRecipeEffects {
         mergeMap(([_, recipe, userId]) => {
             recipe = { ...recipe, dateCreated: new Date() }
             return this.recipesdata.add$(userId, recipe.id, recipe).pipe(
-                switchMap(() => [newRecipeAddSuccess({ recipe }), alertAddSuccess(`${recipe.name} was added to your recipes.`, routeFoodRecipe(recipe.id))]),
+                switchMap(() => [
+                    newRecipeAddSuccess({ recipe }),
+                    alertAddSuccess(`${recipe.name} was added to your recipes.`, routeFoodRecipe(recipe.id)),
+                    notificationAdd({ notification: { app: FOOD_APP.id, message: `${recipe.name} was added to your recipes.`, link: routeFoodRecipe(recipe.id) } })
+                ]
+                ),
                 tap(() => this.router.navigateByUrl(routeFoodRecipe(recipe.id))),
                 catchError(() => of(newRecipeAddError()))
             )
