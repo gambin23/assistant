7ffb51f52e80ebb2a7cdc84eefd4c19a155b6669
Gilbert Gambin <gilbertgambin@gmail.com>
2022-12-29 22:30:35 +0100

    fix(food): remove exports that are not necessary in store

--- src/food/src/store/index.ts
+++ src/food/src/store/index.ts
@@ -1,35 +1,29 @@
 export * from './store';
 
-export * from './calendar/calendar.actions';
+export { CalendarActions } from './calendar/calendar.actions';
 export * from './calendar/calendar.effects';
 export * from './calendar/calendar.functions';
 export * from './calendar/calendar.model';
 export * from './calendar/calendar.reducer';
-export * from './calendar/calendar.selector';
+export { CalendarSelector } from './calendar/calendar.selector';
 
-export * from './categories/categories.actions';
+export { CategoriesActions } from './categories/categories.actions';
 export * from './categories/categories.effects';
 export * from './categories/categories.model';
 export * from './categories/categories.reducer';
-export * from './categories/categories.selector';
+export { CategoriesSelector } from './categories/categories.selector';
 
-export * from './categories/categories.actions';
-export * from './categories/categories.effects';
-export * from './categories/categories.model';
-export * from './categories/categories.reducer';
-export * from './categories/categories.selector';
-
-export * from './new-recipe/new-recipe.actions';
+export { NewRecipeActions } from './new-recipe/new-recipe.actions';
 export * from './new-recipe/new-recipe.effects';
 export * from './new-recipe/new-recipe.model';
 export * from './new-recipe/new-recipe.reducer';
-export * from './new-recipe/new-recipe.selector';
+export { NewRecipeSelector } from './new-recipe/new-recipe.selector';
 
-export * from './recipes/recipes.actions';
+export { RecipesActions } from './recipes/recipes.actions';
 export * from './recipes/recipes.effects';
 export * from './recipes/recipes.model';
 export * from './recipes/recipes.reducer';
-export * from './recipes/recipes.selector';
+export { RecipesSelector } from './recipes/recipes.selector';
 
-export * from './search/search.actions';
-export * from './search/search.selector';
+export { SearchActions } from './search/search.actions';
+export { SearchSelector } from './search/search.selector';
